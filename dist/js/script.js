42b60e92f40c0b5e9b5e2ff589a09258bbbc42bb
khairil45 <khairilcandra285@gmail.com>
2023-09-17 11:08:23 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Klik diluar Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// firebase
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        dark: "#18181b",
+      },
+    },
+  },
+  plugins: [],
+};
+``;
